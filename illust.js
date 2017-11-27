3b63777dfc99a0ba4d11a0a0a54f65e34f492d50
steliviere <32293736+steliviere@users.noreply.github.com>
2017-11-28 05:12:33 +0900

    Update illust.js

--- illust.js
+++ illust.js
@@ -1,8 +1,10 @@
 var sky, in_bg;
+var walk;
 var r=0;
 function preload() {
   sky = loadImage("source/sky.png");
   in_bg = loadImage("source/in_bg.png");
+  walk = new Animation("source/walk-",60);
 }
 function setup() {
   createCanvas(windowWidth,windowHeight);
@@ -11,13 +13,33 @@ function draw()
 {
   push();
   translate(width/2,height/2);
-  display_mid(in_bg);
+  push();
   rotate(r);
   display_mid(sky);
   pop();
+  display_mid(in_bg);
+  walk.display(-3,176);
+  pop();
   r+=0.1;
 }
+
 function display_mid(img)
 {
   image(img, -img.width/2,-img.height/2);
+}
+function Animation(prefix, count)
+{
+	this.sprite=[];
+	this.imageCount=count;
+	for(var i=0;i<this.imageCount;i++)
+	{
+		var filename=prefix+(i+1)+".png";
+		this.sprite[i]=loadImage(filename);
+	}
+	this.frame=0;
+	this.display=function(x,y)
+	{
+		this.frame=(this.frame+1)%this.imageCount;
+		display_mid(this.sprite[this.frame],x,y);
+	}
 }
