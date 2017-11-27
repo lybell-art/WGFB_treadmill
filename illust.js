0c4398c8d49b71ce19d3aabdad809c9aa21a94ad
steliviere <32293736+steliviere@users.noreply.github.com>
2017-11-27 14:53:59 +0900

    Create illust.js

---
+++ illust.js
@@ -0,0 +1,17 @@
+var img;
+var r;
+function preload() {
+  img = loadImage("source/sky.png");
+}
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+}
+function draw()
+{
+  push();
+  translate(width/2,height/2);
+  rotate(r);
+  image(img,-img.width/2,-img.height/2);
+  pop();
+  r++;
+}
