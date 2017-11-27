0fc2c46fb443ca959be08c7b56f862790c33ff89
steliviere <32293736+steliviere@users.noreply.github.com>
2017-11-28 05:47:57 +0900

    Update illust.js

--- illust.js
+++ illust.js
@@ -1,6 +1,7 @@
 var sky, in_bg;
 var walk;
 var r=0;
+var gazer=1;
 function preload() {
   sky = loadImage("source/sky.png");
   in_bg = loadImage("source/in_bg.png");
@@ -8,9 +9,11 @@ function preload() {
 }
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  walk.before=millis();
 }
 function draw()
 {
+  var speed=1+gazer*0.5;
   push();
   translate(width/2,height/2);
   push();
@@ -18,9 +21,10 @@ function draw()
   display_mid(sky);
   pop();
   display_mid(in_bg);
+  walk.setSped(speed);
   walk.display(-3,177);
   pop();
-  r+=radians(8/60);
+  r+=radians(8/60*speed);
 }
 
 function display_mid(img)
@@ -39,9 +43,16 @@ function Animation(prefix, count)
 	this.frame=0;
 	this.width=130;
 	this.height=320;
+	this.fps=60;
+	this.before=0;
 	this.display=function(x,y)
 	{
-		this.frame=(this.frame+1)%this.imageCount;
+		this.frame=(this.frame+int((millis()-this.before)*this.fps/1000))%this.imageCount;
 		image(this.sprite[this.frame],x-this.width/2,y-this.height/2);
+		this.before=millis();
+	}
+	this.setSpeed=function(n)
+	{
+		this.fps=60*n;
 	}
 }
